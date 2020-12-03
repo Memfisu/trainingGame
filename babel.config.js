b19e7ad87492e8bbff6b32ac96fcee084ac65407
GasAR <A.Gasymova@bssys.com>
2020-12-04 00:00:24 +0400

    Start - 3

---
+++ babel.config.js
@@ -0,0 +1,50 @@
+const envDevelopment = [
+  '@babel/env',
+  {
+    targets: {
+      browsers: [
+        'Chrome >= 52',
+        'FireFox >= 44',
+        'Safari >= 8',
+        'Explorer 11',
+        'last 4 Edge versions'
+      ]
+    },
+    modules: false,
+    useBuiltIns: 'usage'
+  },
+  'react-hot-loader/babel'
+];
+
+const envProduction = ['@babel/env', { useBuiltIns: 'usage' }];
+
+module.exports = ({ env }) => {
+  const isDev = env() === 'development';
+
+  const presets = [
+    isDev ? envDevelopment : envProduction,
+    '@babel/preset-react'
+  ];
+
+  const plugins = [
+    '@babel/plugin-transform-runtime',
+    [
+      {
+        displayName: false,
+        ssr: false,
+        minify: !isDev,
+        pure: !isDev
+      }
+    ],
+    ['lodash', { id: ['recompose'] }],
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator'
+  ];
+
+  return {
+    presets,
+    plugins
+  };
+};
